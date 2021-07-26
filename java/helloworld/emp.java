009885103ad590fd2aa5606997baebd93f461fc4
小白李 <74003709+woodson3stones@users.noreply.github.com>
2021-07-26 18:13:55 +0800

    java practise

---
+++ java/helloworld/emp.java
@@ -0,0 +1,23 @@
+public class emp extends people implements my_interface {
+	private String profession;
+	public emp(){
+		super();
+		profession="worker";
+	}
+	public emp(String name ,int old,String profession){
+		super(name,old);
+		this.profession=profession;
+	}
+	public void show(){
+		//super.show();
+		System.out.println("i am a worker!!!!");
+		System.out.println("profession:"+profession);
+	}
+	public void work(){
+		//super.show();
+		System.out.println("Just DO it !");
+	}
+	public void get_married(){
+		System.out.println("i am not happy although i get married ,i want more money!");
+	}
+}
