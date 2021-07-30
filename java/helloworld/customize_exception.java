40466b720897292acfcf2bf4e4c2b641e6841498
小白李 <74003709+woodson3stones@users.noreply.github.com>
2021-07-30 18:13:38 +0800

    Add files via upload

---
+++ java/helloworld/customize_exception.java
@@ -0,0 +1,6 @@
+public class customize_exception extends Exception{//compile exception need try catch 
+	public customize_exception(){}
+	public customize_exception(String message){
+		super(message);
+	}
+}
