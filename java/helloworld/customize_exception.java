ea53119325c9ea0c1844fadc39d6e2381f6d19f5
小白李 <74003709+woodson3stones@users.noreply.github.com>
2021-08-13 11:23:19 +0800

    Delete java/helloworld directory

--- java/helloworld/customize_exception.java
+++
@@ -1,6 +0,0 @@
-public class customize_exception extends Exception{//compile exception need try catch 
-	public customize_exception(){}
-	public customize_exception(String message){
-		super(message);
-	}
-}
--- java/helloworld/my_interface.java
+++
@@ -1,4 +0,0 @@
-public interface my_interface {
-	public static final boolean married=true; //<=> constant value
-	public abstract void get_married();//<=> void get_married();
-}
